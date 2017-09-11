aaef241ed8864debaf646fb2fd494357dd0b71e6
angpulangpluma <mariennelopez30@gmail.com>
2017-09-11 17:00:23 +0800

    completed test file handling
    - created function to initiate which algorithm is to be tested
    - created function to initiate test log file
    - created function to input test times for encrypting text
    - created function to input test times for encrypting files
    - created function to input test times to decrypting text
    - created function to input test times to decrypt files

--- src/enc_mods/time_algo.java
+++ src/enc_mods/time_algo.java
@@ -6,7 +6,9 @@
 
 package enc_mods;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 /**
  *
@@ -17,22 +19,15 @@ public class time_algo {
 //    private aes basealgo;
     private text_aes textalgo;
     private file_aes filealgo;
+    private FileWriter fw;
+    private BufferedWriter bw;
     
-//    public time_algo(int n){
-//       
-////       basealgo = null;
-//       textalgo = null;
-//       filealgo = null;
-//       switch(n){
-////           case 1:
-////            basealgo = new aes(); break;
-//           case 2:
-//            textalgo = new text_aes(); break;
-//           case 3:
-//            filealgo = new file_aes(); break;
-//       }
-//       
-//    }
+    public time_algo(){
+        this.textalgo = null;
+        this.filealgo = null;
+        this.fw = null;
+        this.bw = null;
+    }
     
     public time_algo(text_aes ta){
         this.textalgo = ta;
@@ -40,6 +35,86 @@ public class time_algo {
     
     public time_algo(file_aes fa){
         this.filealgo = fa;
+    }
+    
+    public void setFileLog(FileWriter fw) throws Exception{
+        this.fw = fw;
+        this.bw = new BufferedWriter(this.fw);
+        writeNewLine();
+        //write current time of entry
+        if (textalgo!=null)
+            bw.write("testing text encryption algorithm");
+        if (filealgo!=null)
+            bw.write("testing file encryption algorithm");
+        writeNewLine();
+        //find out what bw.flush() does again???
+        bw.flush();
+    }
+    
+    public void writeNewLine() throws Exception{
+        bw.write("-------");
+        bw.newLine();
+        //find out what bw.flush() does again???
+        bw.flush();
+    }
+    
+    public void writeEncTime(int n, File file, String string) throws Exception{
+        writeNewLine();
+        switch(n){
+            case 21: //aes encryption - text
+             {
+                bw.write("testing aes text encryption");
+                bw.write("input: " + string);
+                bw.write("output: " + textalgo.getEncString(string));
+                bw.write("time elapsed: " + getEncTime(n, file, string));
+                writeNewLine();
+                bw.flush();
+             }; break;
+            case 22: //hash encryption - text
+             {
+                bw.write("testing aes text encryption");
+                bw.write("input: " + string);
+                bw.write("output: " + textalgo.getHashedString(string));
+                bw.write("time elapsed: " + getEncTime(n, file, string));
+                writeNewLine();
+                bw.flush();
+             }; break;
+            case 3: //aes encryption - file
+             {
+                bw.write("testing aes text encryption");
+                //find out how to know filesize through java
+                //before encrypting filesize
+                //after encrypting filesize
+                bw.write("time elapsed: " + getEncTime(n, file, string));
+                writeNewLine();
+                bw.flush();
+             }; break;
+        }
+    }
+    
+    public void writeDecTime(int n, File file, String string) throws Exception{
+        writeNewLine();
+        switch(n){
+            case 2: //aes encryption - text
+             {
+                bw.write("testing aes text encryption");
+                bw.write("input: " + string);
+                bw.write("output: " + textalgo.getDecString(string));
+                bw.write("time elapsed: " + getDecTime(n, file, string));
+                writeNewLine();
+                bw.flush();
+             }; break;
+            case 3: //aes encryption - file
+             {
+                bw.write("testing aes text encryption");
+                //find out how to know filesize through java
+                //before encrypting filesize
+                //after encrypting filesize
+                bw.write("time elapsed: " + getDecTime(n, file, string));
+                writeNewLine();
+                bw.flush();
+             }; break;
+        }
     }
     
     public long getEncTime(int n, File file, String string){
@@ -93,5 +168,14 @@ public class time_algo {
         
         return endtime - starttime;
     }
+    
+    public void finishTest() throws Exception{
+        if (bw!=null)
+            bw.close();
+        if (fw!=null)
+            fw.close();
+    }
+    
+    
 
 }
