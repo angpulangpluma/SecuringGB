361a3c39f0712b6aba3af954f133ad129764a245
angpulangpluma <mariennelopez30@gmail.com>
2017-09-13 11:09:07 +0800

    new file: test_enc_mods
    - created runnable for test_module

---
+++ src/enc_mods_test/test_enc_mods.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package enc_mods_test;
+
+/**
+ *
+ * @author Marienne Lopez
+ */
+public class test_enc_mods {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new test_module().setVisible(true);
+    }
+
+}
